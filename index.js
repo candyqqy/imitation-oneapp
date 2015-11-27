18831c98b3af7130ff414e9fe2385efbd5aa6ebb
QQY <candy_dr_qqy@hotmail.com>
2015-11-27 15:06:43 +0800

    modify

--- index.js
+++ index.js
@@ -17,58 +17,78 @@ app.listen(3000, function () {
 });
 
 app.get('/', function (req, res) {
+    //var oneUrl = '';
     // 用 superagent 去抓取 http://wufazhuce.com/one/vol.1139 的内容
     // superagent.get('http://wufazhuce.com/one/vol.1143')
-    superagent.get('http://wufazhuce.com/one/vol.1145')
+    superagent.get('http://wufazhuce.com/')
         .end(function (err, sres) {
             // 常规的错误处理
             if (err) {
                 return next(err);
             }
+            var _$ = cheerio.load(sres.text);
+            var oneUrl = _$('.carousel-inner .item:first-child a').attr('href');
+            console.log(oneUrl);
+            superagent.get(oneUrl)
+                .end(function (err, sres) {
+                    // 常规的错误处理
+                    if (err) {
+                        return next(err);
+                    }
 
-            // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
-            // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
-            // 剩下就都是 jquery 的内容了
+                    // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
+                    // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
+                    // 剩下就都是 jquery 的内容了
 
-            var $ = cheerio.load(sres.text);
+                    var $ = cheerio.load(sres.text);
 
-            var articlePrincipal = $('.articulo-principal').text();
-            var articleTitle = $('.articulo-titulo').text();
-            var articleAuthor = $('.articulo-autor').text();
-            var articleContent = [];
-            $('.one-articulo .articulo-contenido p').each(function (item, element) {
-                var $element = $(element);
-                articleContent.push({
-                    content: $element.text()
+                    var articlePrincipal = $('.articulo-principal').text();
+                    var articleTitle = $('.articulo-titulo').text();
+                    var articleAuthor = $('.articulo-autor').text();
+                    var articleContent = [];
+                    $('.one-articulo .articulo-contenido p').each(function (item, element) {
+                        var $element = $(element);
+                        articleContent.push({
+                            content: $element.text()
+                        });
+                    });
+                    var questionTitle = $('.one-cuestion h4:nth-child(2)').text();
+                    var questionAsk = $('.cuestion-contenido').text();
+                    var questionAnswer = $('.one-cuestion h4:nth-child(6)').text();
+                    var questionContent = [];
+                    $('.cuestion-contenido p').each(function (item, element) {
+                        var $element = $(element);
+                        questionContent.push({
+                            content: $element.text()
+                        });
+                    });
+
+                    var vol = $('.one-titulo').text();
+                    var day = $('.one-pubdate .dom').text();
+                    var year = $('.one-pubdate .may').text();
+                    var motto = $('.one-cita').text();
+                    res.render('index', {
+                        title: 'One 一个',
+                        articleContent: articleContent,
+                        articlePrincipal: articlePrincipal,
+                        articleTitle: articleTitle,
+                        articleAuthor: articleAuthor,
+                        questionTitle: questionTitle,
+                        questionAsk: questionAsk,
+                        questionAnswer: questionAnswer,
+                        questionContent: questionContent,
+                        vol: vol,
+                        day: day,
+                        year: year,
+                        motto: motto
+                    });
                 });
-            });
-            var questionTitle = $('.one-cuestion:nth-child(2)').text();
-            var questionAsk = $('.cuestion-contenido').text();
-            var questionAnswer = $('.one-cuestion h4:nth-child(5)').text();
-            var questionContent = [];
-            $('.cuestion-contenido p').each(function (item, element) {
-                var $element = $(element);
-                questionContent.push({
-                    content: $element.text()
-                });
-            });
-
-            res.render('index', {
-                title: 'One 一个',
-                articleContent: articleContent,
-                articlePrincipal: articlePrincipal,
-                articleTitle: articleTitle,
-                articleAuthor: articleAuthor,
-                questionTitle:questionTitle,
-                questionAsk:questionAsk,
-                questionAnswer:questionAnswer,
-                questionContent:questionContent
-            });
         });
-    //res.render('index', {
-    //    title: 'One首页'
-    //});
-    //res.send('Hello World!');
-});
+//res.render('index', {
+//    title: 'One首页'
+//});
+//res.send('Hello World!');
+})
+;
 
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,20 +2,20 @@
  * Created by qqy on 15/11/26.
  */
 $(document).ready(function () {
+
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         var hash = $(e.target).attr('href');
-        if (hash == "#images") {
+        if (hash == "#index") {
             hash = "";
-        } else {
-            hash = hash.replace('#tab-', '#');
         }
-        $('#vanilla-comments').detach().prependTo($(e.target).attr('href') + ' .one-comentarios')
+        //$('#vanilla-comments').detach().prependTo($(e.target).attr('href') + ' .one-comentarios')
         location.hash = hash;
         window.scrollTo(0, 0);
     })
 
     if (location.hash != "") {
-        $('#one-navbar a[href=' + location.hash.replace('#', '#tab-') + ']').tab('show');
+        $('.one-navbar a[aria-controls="index"]').tab('show');
+        //$('#one-navbar a[href='+location.hash.replace('#', '#tab-')+']').tab('show');
         window.scrollTo(0, 0);
     }
 });
